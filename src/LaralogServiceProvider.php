3e149fb95480e0baed00dfd17950068cccb774da
Valentin Prugnaud <valentin@whatdafox.com>
2016-05-04 00:21:24 -0700

    Add config file

--- src/LaralogServiceProvider.php
+++ src/LaralogServiceProvider.php
@@ -2,11 +2,35 @@
 
 namespace Laralog;
 
+use Illuminate\Support\ServiceProvider;
+
 /**
  * Class LaralogServiceProvider
  * @package Laralog
  */
-class LaralogServiceProvider
+class LaralogServiceProvider extends ServiceProvider
 {
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__.'/config/laralog.php' => config_path('laralog.php'),
+        ]);
+    }
 
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->mergeConfigFrom(
+            __DIR__.'/config/laralog.php', 'laralog'
+        );
+    }
 }
