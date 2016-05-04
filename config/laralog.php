0ae35cb8a2ebabb11067cba3924199df6e4ea2e4
Valentin Prugnaud <valentin@whatdafox.com>
2016-05-04 02:41:48 -0700

    Add comments in configuration

--- config/laralog.php
+++ config/laralog.php
@@ -1,12 +1,45 @@
 <?php
 
 return [
-
+    /**
+     * --------------------------------------------------------------------------
+     * Environment Handlers
+     * --------------------------------------------------------------------------
+     *
+     * Configure the different handlers for each environment. Each environment
+     * should be correctly configured within the handlers section.
+     *
+     * Example:
+     *
+     *      'local' => ['slack']
+     *
+     */
     'environments' => [
         'local' => ['slack']
     ],
 
+    /**
+     * --------------------------------------------------------------------------
+     * Handlers Configuration
+     * --------------------------------------------------------------------------
+     *
+     * Configure the different handlers that your application support.
+     *
+     * Currently supported:
+     *  - Slack
+     *
+     */
     'drivers' => [
+        /**
+         * --------------------------------------------------------------------------
+         * Slack integration
+         * --------------------------------------------------------------------------
+         *
+         *  To obtain an api key, visit: https://api.slack.com/web#auth
+         *
+         * IMPORTANT: You channel name must be prefixed the # symbol
+         *
+         */
         'slack' => [
             'api_key' => 'YOUR_SLACK_API_KEY',
             'channel' => '#general'
